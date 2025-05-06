713ee4d680c98342ca1ce878b9a0c4cff9e46d0c
Marcos <72473635+marcossrlv@users.noreply.github.com>
2025-05-06 17:37:46 +0200

    Actualiza los Dockerfiles y el docker-compose para corregir rutas de copia y mejorar la configuración de servicios.

--- backend/kafka/producer/run_producer.sh
+++ backend/kafka/producer/run_producer.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 echo "⏰ Ejecutando producer.py a las $(date)"
 
-# Export environment variables
-set -a
-source /app/.env
-set +a
-
-# Run producer.py and redirect stdout and stderr to Docker logs
 /usr/local/bin/python /app/producer.py
