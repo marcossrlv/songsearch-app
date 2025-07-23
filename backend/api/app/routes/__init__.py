cf87ad21ec8ce28e08da6d80f6eb0c4b91cffe58
Marcos <72473635+marcossrlv@users.noreply.github.com>
2025-07-23 15:01:51 +0200

    Agrega modo de depuración en la ejecución de la aplicación y registra nuevas rutas de autenticación y usuario.

--- backend/api/app/routes/__init__.py
+++ backend/api/app/routes/__init__.py
@@ -1,6 +1,10 @@
 from .recommend import bp_recommend
 from .playlists import bp_playlists
+from .auth_routes import auth_bp
+from .user_routes import user_bp
 
 def register_blueprints(app):
     app.register_blueprint(bp_recommend)
     app.register_blueprint(bp_playlists)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(user_bp)
